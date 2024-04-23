d8f129af5470ccf55d3ee690e105552aca50759d
HKanson1234 <112676843+HKanson1234@users.noreply.github.com>
2024-04-23 18:46:48 +0800

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "5.25.0"
-    }
-  }
-}
-
-provider "google" {
-  project     = "terraformproject-415508"
-  region      = "asia-east2"
-}
-
 resource "google_compute_network" "my_vpc" {
   name                    = "my-gcp-vpc"
   auto_create_subnetworks = false
@@ -26,8 +12,7 @@ resource "google_compute_subnetwork" "subnet-1" {
 
 resource "google_compute_subnetwork" "subnet-2" {
   name          = "subnet-2"
-  ip_cidr_range = "10.0.1.0/24"
+  ip_cidr_range = "10.0.2.0/24"
   region        = "asia-east2"
   network       = google_compute_network.my_vpc.id
 }
-
