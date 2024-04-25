1e9cb0fc5505dd4865a2fa786f29b01a7d5682da
HKanson1234 <112676843+HKanson1234@users.noreply.github.com>
2024-04-25 20:26:23 +0800

    Update gke.tf

--- gcp/gke.tf
+++ gcp/gke.tf
@@ -1,1 +1,28 @@
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "asia-east2-a"
 
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  }
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "asia-east2-a"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
