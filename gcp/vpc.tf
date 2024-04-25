9bf80714271a0cb8994a3ad669bbac196e2b82ce
HKanson1234 <112676843+HKanson1234@users.noreply.github.com>
2024-04-25 13:43:43 +0800

    Update vpc.tf

--- gcp/vpc.tf
+++ gcp/vpc.tf
@@ -1,18 +1,45 @@
-resource "google_compute_network" "terraform_vpc" {
-  name                    = "terraform_vpc"
-  auto_create_subnetworks = false
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+        
+        kubernetes = {
+            source = "hashicorp/kubernetes"
+            version = "~> 2.0"
+        }
+        helm = {
+            source = "hashicorp/helm"
+            version = "2.13.0"
+        }
+        google = {
+            source = "hashicorp/google"
+            version = "5.25.0"
+        }
+        google-beta = {
+            source = "hashicorp/google-beta"
+            version = "5.26.0"
+        }
+    }
+    required_version = ">= 1.4.7"
 }
-#private subnet 1
-resource "google_compute_subnetwork" "private_subnet_1" {
-  name          = "private_subnet-1"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = "asia-east2"
-  network       = google_compute_network.terraform_vpc.id
+
+provider "aws" {
+  region = "us-east-1"
 }
-#private  subnet 2
-resource "google_compute_subnetwork" "private_subnet_2" {
-  name          = "private_subnet_2"
-  ip_cidr_range = "10.0.2.0/24"
-  region        = "asia-east2"
-  network       = google_compute_network.terraform_vpc.id
+
+provider "google" {
+ # credentials = "terraformproject-415508-05982ee89e2a.json"
+  project     = "terraformproject-415508"
+  region      = "asia-east2"
+  zone        = "asia-east2-a"
 }
+
+provider "google-beta" {
+  #credentials = "terraformproject-415508-05982ee89e2a.json"
+  project     = "terraformproject-415508"
+  region      = "asia-east2"
+  zone        = "asia-east2-a"
+}
+provider "azurerm" {}
