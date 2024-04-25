85c0adb7047d973672b4b04aebd7bf7d3ecce316
HKanson1234 <112676843+HKanson1234@users.noreply.github.com>
2024-04-25 15:04:35 +0800

    Update vpc.tf

--- gcp/vpc.tf
+++ gcp/vpc.tf
@@ -29,3 +29,65 @@ resource "google_compute_subnetwork" "private2" {
   region        = "asia-east2"
   network       = google_compute_network.terraformvpc.id
 }
+
+####################################Below code need to run 2 apply and 2 destroy for working
+
+resource "google_compute_router" "nat" {
+
+  project = "terraformproject-415508"
+
+  name    = "asia-east2-nat-router"
+  region  = "asia-east2"
+  network = google_compute_network.terraformvpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+
+  project = "terraformproject-415508"
+
+  name                   = "asia-east2-nat"
+  router                 = "asia-east2-nat-router"
+  region                 = "asia-east2"
+  nat_ip_allocate_option = "AUTO_ONLY"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = "private1"
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+    subnetwork {
+    name                    = "private2"
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+################# Testing code
+# allows google instances to resolve aws private domains
+resource "google_dns_managed_zone" "aws" {
+  provider = google-beta
+
+  project = "terraformproject-415508"
+
+  name        = "aws"
+  description = "private dns zone to enable resolving ec2 private domains"
+
+  dns_name = "${var.aws_dns_suffix}."
+
+  visibility = "private"
+
+  private_visibility_config {
+    networks {
+      network_url =  google_compute_network.main.self_link
+    }
+  }
+
+  forwarding_config {
+    target_name_servers {
+      ipv4_address = var.aws_dns_ip_addresses[0]
+    }
+
+    target_name_servers {
+      ipv4_address = var.aws_dns_ip_addresses[1]
+    }
+  }
+}
